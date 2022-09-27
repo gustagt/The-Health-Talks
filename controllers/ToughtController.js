e53e27e1b3b4022ec1e0c7ef5759a3f09de79232
osamukafps <samuelhplt@gmail.com>
2022-09-26 21:54:23 -0300

    inclusão e exclusao de pensamentos

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -6,4 +6,74 @@ module.exports = class ToughtController {
     static async showToughts(req, res){
         res.render('toughts/home')
     }
+
+    static async dashboard(req, res){
+
+        const userId = req.session.userid;
+
+        const user = await User.findOne({ where: {id: userId }, include: Tought, plain: true,});
+
+        if(!user){
+            res.redirect('/login');
+        }
+
+        const toughts = user.Toughts.map((result) => result.dataValues);
+        let emptyToughts = false;
+        
+        if(toughts.length === 0){
+            emptyToughts = true;
+        }
+
+        res.render('toughts/dashboard', { toughts, emptyToughts });
+    }
+
+    static createTought(req, res){
+        res.render('toughts/create');
+    }
+
+    static async createToughtPost(req, res){
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid,
+        }
+
+        await Tought.create(tought);
+
+        req.flash('message', 'Criado!');
+
+        try
+        {
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard');
+            })
+
+        }
+        catch(err)
+        {
+            console.log('Error: ' + err);
+        }
+        
+        
+    }
+
+    static async removeTought(req, res){
+        const toughId = req.body.id;
+        const userId = req.session.userid;
+
+        try
+        {
+            await Tought.destroy({ where: {id: toughId, UserId: userId}});
+
+            req.flash('message', 'Pensamento removido!');
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard');
+            })
+
+        }
+        catch(error){
+            console.log(error);
+        }
+
+        
+    }
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const ToughtController = require('../controllers/ToughtController');
 
+//HELPER
+const checkAuth = require('../helpers/auth').checkAuth;
+
+router.get('/dashboard', checkAuth, ToughtController.dashboard);
 router.get('/', ToughtController.showToughts);
+router.get('/add', checkAuth, ToughtController.createTought);
+
+router.post('/add', checkAuth, ToughtController.createToughtPost);
+router.post('/remove', checkAuth, ToughtController.removeTought);
 
 module.exports = router;
